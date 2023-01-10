74a61c58e629e575c7d48d9cb463b98f3e00a12c
MariaBrFarias <110403184+MariaBrFarias@users.noreply.github.com>
2023-01-10 00:50:09 -0300

    1 file modified, 5 added

---
+++ src/app/buton/buton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buton',
+  templateUrl: './buton.component.html',
+  styleUrls: ['./buton.component.css']
+})
+export class ButonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/menubarra/menubarra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menubarra',
+  templateUrl: './menubarra.component.html',
+  styleUrls: ['./menubarra.component.css']
+})
+export class MenubarraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
